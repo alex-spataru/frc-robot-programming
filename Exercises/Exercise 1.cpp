798374bda08d91d07ff2d28977a0248590793618
Alex Spataru <alex.racotta@gmail.com>
2015-05-16 09:47:08 -0500

    Update images and chapters

---
+++ Exercises/Exercise 1.cpp
@@ -0,0 +1,15 @@
+/*
+  Name: Exercise 1
+  Author: Alex Spataru
+  Description: C++ programming exercise 1
+  Date: 5/16/2015
+  Copyright: Public domain
+*/	
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
